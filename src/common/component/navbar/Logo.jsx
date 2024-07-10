cc808dc54797bba7a1a9b18fca720666667e44a6
HinaSakina <hinasakina4@gmail.com>
2024-07-10 12:40:20 +0500

    Set Logo

--- src/common/component/navbar/Logo.jsx
+++ src/common/component/navbar/Logo.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "../element/Image";
+import { useTheme } from 'next-themes';
 
 export default function Logo() {
+  const { theme } = useTheme();
   return (
     <>
       <Image
-        className="w-[50%] left-[-10%] top-[-23%] absolute h-auto object-cover  rounded-3xl"
-        src="/Finger.svg"
+        className="w-[100%]  h-auto object-cover bg-transparent  rounded-3xl"
+        src={theme === 'light' ? '/WordLogoLightBg.svg' : '/WordLogo.svg'}
         alt="Image"
         width={400}
         height={400}
+        
         priority
       />
     </>
--- src/common/component/navbar/Navbar.jsx
+++ src/common/component/navbar/Navbar.jsx
@@ -9,6 +9,7 @@ import { usePathname } from "next/navigation";
 import dynamic from "next/dynamic";
 import { ButtonSpot } from "../element/ButtonSpot";
 import DarkmodeSwitch from "../element/DarkmodeSwitch";
+import Logo from "./Logo";
 
 // const Logos = dynamic(() => import('./Logo'), {
 //   ssr: false
@@ -16,7 +17,7 @@ import DarkmodeSwitch from "../element/DarkmodeSwitch";
 
 const Navbar = () => {
   const pathname = usePathname();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const [scroll, setScrolled] = useState(false);
 
   const handleOpen = () => {
@@ -36,30 +37,23 @@ const Navbar = () => {
     <motion.div
       className={clsx(
         scroll ? "backdrop-blur-sm" : "",
-        `w-full transition-all duration-300 mx-auto bg-[#ffffffd0] dark:bg-[#000000d0]  max-w-[1500px]  border-b-[1px] border-neutral-300 dark:border-neutral-800 py-5 px-5 2xl:rounded-3xl  lg:px-10 fixed top-0 z-[99]`
+        `w-full transition-all duration-300 mx-auto bg-[#ffffffd0] dark:bg-[#000000d0] max-w-[1500px] border-b-[1px] border-neutral-300 dark:border-neutral-800 py-5 px-5 2xl:rounded-3xl lg:px-10 fixed top-0 z-[99]`
       )}
     >
       <div className="flex flex-row justify-between items-center">
-        <div className="flex  z-[999] items-start justify-start">
+        <div className="flex z-[999] items-center justify-start">
           <Link
             href="/"
-            className="group relative w-auto flex justify-start items-start"
+            className=" flex items-center"
           >
-            {/* <Logos /> */}
-            <h1
-              className={`${
-                open ? "text-white" : ""
-              }  text-3xl tracking-[-0.10em] font-bold`}
-            >
-              Teaser
-            </h1>
+            <Logo />
           </Link>
         </div>
         <div className="lg:flex hidden justify-center gap-10 items-center w-full">
           {NavigationItem.map((item, index) => (
             <Link
               href={item.src}
-              className="group  flex justify-center gap-5 items-center  relative"
+              className="group flex justify-center gap-5 items-center relative"
               key={index}
             >
               <h1 className="text-base font-medium text-black dark:text-white line-clamp-1">
@@ -68,15 +62,15 @@ const Navbar = () => {
               <div
                 className={clsx(
                   pathname === item.src
-                    ? "absolute top-[23px] h-[2px] flex items-center w-[50%]  bg-[#000] dark:bg-white  transition-transform duration-300 "
-                    : "absolute top-[23px] h-[2px] w-[0%]  bg-[#000] dark:bg-white  transition-all duration-300 group-hover:w-[50%]"
+                    ? "absolute top-[23px] h-[2px] flex items-center w-[50%] bg-[#000] dark:bg-white transition-transform duration-300"
+                    : "absolute top-[23px] h-[2px] w-[0%] bg-[#000] dark:bg-white transition-all duration-300 group-hover:w-[50%]"
                 )}
               ></div>
             </Link>
           ))}
         </div>
 
-        <div className="lg:hidden flex  items-center">
+        <div className="lg:hidden flex items-center">
           <div className="px-2">
             <DarkmodeSwitch />
           </div>
